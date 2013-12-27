d7e0c8ff6af03b86b61e96209d1664cf9c939130
bryantzhang <bryantzhang1990@gmail.com>
2013-12-27 13:00:38 +0800

    add advanced search modal to vtl files

--- src/main/java/dmsystem/util/StringUtil.java
+++ src/main/java/dmsystem/util/StringUtil.java
@@ -1,5 +1,46 @@
 package dmsystem.util;
 
+/**
+ * 
+ * @author bryant zhang
+ * 
+ */
 public class StringUtil {
+
+	public static boolean equals(String str1, String str2) {
+
+		boolean result = false;
+		if (str1 == null || str2 == null)
+			return result;
+
+		if (str1 != null && str2 != null && str1.equals(str2)) {
+			result = true;
+		}
+
+		return result;
+	}
+
+	public static boolean equalsIgnoreCase(String str1, String str2) {
+
+		boolean result = false;
+		if (str1 == null || str2 == null)
+			return result;
+
+		if (str1 != null && str2 != null && str1.equalsIgnoreCase(str2)) {
+			result = true;
+		}
+
+		return result;
+	}
+
+	public static boolean isNullOrEmpty(String str) {
+
+		if (str == null)
+			return true;
+		if (str.isEmpty())
+			return true;
+
+		return false;
+	}
 
 }
