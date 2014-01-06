21fae3b1e1cb390bf0af8c3dc246d1aa90de2b02
bryantzhang <bryantzhang1990@gmail.com>
2014-01-06 20:05:22 +0800

    modify option value of advanced doc search

--- src/main/java/dmsystem/dao/DocumentSearchDao.java
+++ src/main/java/dmsystem/dao/DocumentSearchDao.java
@@ -59,6 +59,17 @@ public class DocumentSearchDao {
 	@SuppressWarnings("unchecked")
 	public List<Document> advancedSearch(Map<String, String> paramsMap)
 			throws Exception {
+		String title = paramsMap.get("title") == null ? "" : paramsMap
+				.get("title");
+		String author = paramsMap.get("author") == null ? "" : paramsMap
+				.get("author");
+		String keywords = paramsMap.get("keywords") == null ? "" : paramsMap
+				.get("keywords");
+		String publisher = paramsMap.get("publisher") == null ? "" : paramsMap
+				.get("publisher");
+		String publishYear = paramsMap.get("publishYear") == null ? ""
+				: paramsMap.get("publishYear");
+		
 		List<Document> docList = null;
 		Session session = this.hibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
@@ -71,16 +82,11 @@ public class DocumentSearchDao {
 					+ "' and title like ? and author like ? and keywords like ? and publisher like ? and year like ?";
 		}
 		Query query = session.createQuery(sql);
-		query.setString(0, "%" + paramsMap.get("title") == null ? ""
-				: paramsMap.get("title") + "%");
-		query.setString(1, "%" + paramsMap.get("author") == null ? ""
-				: paramsMap.get("author") + "%");
-		query.setString(2, "%" + paramsMap.get("keywords") == null ? ""
-				: paramsMap.get("keywords") + "%");
-		query.setString(3, "%" + paramsMap.get("publisher") == null ? ""
-				: paramsMap.get("publisher") + "%");
-		query.setString(4, "%" + paramsMap.get("publishYear") == null ? ""
-				: paramsMap.get("publishYear") + "%");
+		query.setString(0, "%" + title + "%");
+		query.setString(1, "%" + author + "%");
+		query.setString(2, "%" + keywords + "%");
+		query.setString(3, "%" + publisher + "%");
+		query.setString(4, "%" + publishYear + "%");
 		docList = query.list();
 
 		return docList;
